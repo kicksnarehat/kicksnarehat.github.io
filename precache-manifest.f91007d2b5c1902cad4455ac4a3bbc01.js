44f3c19d22098611506cc1f2f5b8e3ae8d4a3ff0
kicksnarehat <kicksnarehat@gmail.com>
2020-10-19 13:48:52 +0000

    Deploying to gh-pages from  @ 84d3b7ce09362967ccd0bd8f324e0c4e46e71b55 🚀

---
+++ precache-manifest.f91007d2b5c1902cad4455ac4a3bbc01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12afd201c3766c034f4e83683933ede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1776d48f0dc3e90fadec",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a2d88159ae748e6be41",
+    "url": "/static/js/2.b8adaec7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b8adaec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1776d48f0dc3e90fadec",
+    "url": "/static/js/main.d670c457.chunk.js"
+  },
+  {
+    "revision": "ea9451281baea2f358eb",
+    "url": "/static/js/runtime-main.b2ed98ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
